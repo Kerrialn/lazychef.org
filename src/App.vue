9b86b678b5e06ea17a2bf76fedca25fc22cb31e4
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2021-04-21 20:58:35 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar />
     <router-view />
   </div>
 </template>
 
+<script>
+import navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    navbar,
+  },
+};
+</script>
+
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;300;400;700&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Work Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  position: relative;
+  width: 100%;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav">
+    <div class="brand-text"><b>Lazy</b> Chef</div>
+    <div>
+      <router-link tag="a" class="nav-link" to="/" exact>Recipes</router-link>
+      <router-link tag="a" class="nav-link" to="/about" exact
+        >About</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.brand-text {
+  font-size: 18px;
+  padding-left: 20px;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.05);
+  padding: 20px 0px;
+}
+.nav a {
+  text-decoration: none;
+  color: black;
+  padding: 0 15px;
+}
+.nav a:hover {
+  text-decoration: none;
+  color: black;
+}
+
+.nav .active {
+  font-weight: bold;
+}
+</style>
