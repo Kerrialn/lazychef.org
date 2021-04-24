a47af26953ff6f23277bfc65440a9e764cdc2076
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2021-04-24 20:35:03 +0200

    add pork-stir-fry and cost per serving

--- src/store/index.js
+++ src/store/index.js
@@ -11,6 +11,7 @@ export default new Vuex.Store({
         id: 1,
         slug: "pesto-meatballs",
         title: "Pesto meatballs in spinach sauce",
+        cost: 1.70,
         description: "",
         duration: 15,
         image: require('.././assets/images/pesto-meatballs/1.jpg'),
@@ -20,19 +21,19 @@ export default new Vuex.Store({
             src: require('.././assets/images/pesto-meatballs/1.jpg'),
           }
         ],
-        serverings: 2,
+        serverings: 1,
         ingredients:[
           {
             id: 1,
             title: 'mince beef',
-            quantity: 500,
+            quantity: 250,
             measurement: 'g',
             isComplete: false
           },
           {
             id: 2,
             title: 'tagliatelle',
-            quantity: 250,
+            quantity: 125,
             measurement: 'g',
             isComplete: false
           },
@@ -40,20 +41,20 @@ export default new Vuex.Store({
             id: 3,
             title: 'basil pesto',
             quantity: 2,
-            measurement: ' table sppons',
+            measurement: ' tea sppons',
             isComplete: false
           },
           {
             id: 4,
             title: 'spinach',
             quantity: 1,
-            measurement: ' large handful',
+            measurement: ' handful',
             isComplete: false
           },
           {
             id: 5,
             title: 'creme fraiche',
-            quantity: 200,
+            quantity: 100,
             measurement: 'g',
             isComplete: false
           },
@@ -92,14 +93,16 @@ export default new Vuex.Store({
           },
         ],
         tags:[
-          'beef', 'pasta', 'italian'
+          'beef', 'pasta', 'italian', 'comfort food', 'feel good', 'wednesday'
         ],
         isPublished: true,
       },
+
       {
         id: 2,
         slug: "steamed-fish",
         title: "Steamed fish & vegetable rush",
+        cost: 2.00 ,
         description: "",
         duration: 20,
         image: require('.././assets/images/steamed-fish/1.jpg'),
@@ -113,12 +116,12 @@ export default new Vuex.Store({
             src: require('.././assets/images/steamed-fish/2.jpg'),
           }
         ],
-        serverings: 2,
+        serverings: 1,
         ingredients:[
           {
             id: 1,
             title: 'fillet of salmon (but nearly any fish fillet will work)',
-            quantity: 400,
+            quantity: 200,
             measurement: 'g',
             isComplete: false
           },
@@ -153,13 +156,13 @@ export default new Vuex.Store({
           {
             id: 6,
             title: 'broccoli',
-            quantity: 250,
+            quantity: 100,
             measurement: 'g',
             isComplete: false
           },
           {
             id: 7,
-            title: 'potatoes',
+            title: 'small potatoes',
             quantity: 3,
             measurement: ' pcs',
             isComplete: false
@@ -194,10 +197,134 @@ export default new Vuex.Store({
          
         ],
         tags:[
-          'fish', 'salmon', 'steamed', 'healthy', 'vegetables'
+          'fish', 'salmon', 'steamed', 'healthy', 'vegetables', 'monday'
         ],
         isPublished: true,
       },
+
+      {
+        id: 3,
+        slug: "pork-stir-fry",
+        title: "Pork fillet stir fry",
+        description: "",
+        duration: 15,
+        image: require('.././assets/images/pork-stir-fry/4.jpg'),
+        images:[
+          {
+            id: 1,
+            src: require('.././assets/images/pork-stir-fry/1.jpg'),
+          },
+          {
+            id: 2,
+            src: require('.././assets/images/pork-stir-fry/2.jpg'),
+          },
+          {
+            id: 3,
+            src: require('.././assets/images/pork-stir-fry/3.jpg'),
+          },
+          {
+            id: 4,
+            src: require('.././assets/images/pork-stir-fry/4.jpg'),
+          }
+        ],
+        serverings: 1,
+        ingredients:[
+          {
+            id: 1,
+            title: 'pork filet',
+            quantity: 100,
+            measurement: 'g',
+            isComplete: false
+          },
+          {
+            id: 2,
+            title: 'broccoli',
+            quantity: 100,
+            measurement: 'g',
+            isComplete: false
+          },
+          {
+            id: 3,
+            title: 'pak choi',
+            quantity: 'half of one',
+            measurement: '',
+            isComplete: false
+          },
+          {
+            id: 5,
+            title: 'garlic',
+            quantity: 4,
+            measurement: ' cloves',
+            isComplete: false
+          },
+          {
+            id: 6,
+            title: 'carrot',
+            quantity: 1,
+            measurement: ' pc',
+            isComplete: false
+          },
+          {
+            id: 7,
+            title: 'ginger',
+            quantity: 50,
+            measurement: 'g',
+            isComplete: false
+          },
+          {
+            id: 8,
+            title: 'dry noddles',
+            quantity: 2,
+            measurement: ' nests',
+            isComplete: false
+          },
+          {
+            id: 9,
+            title: 'soy sauce',
+            quantity: 50 ,
+            measurement: 'ml',
+            isComplete: false
+          },
+        ],
+        steps: [
+          {
+            id: 1,
+            action: "Peel carrot and ginger, then cut up all the vegetables, roughly about the same thickness. (see images for reference)",
+            duration: 7,
+            isComplete: false
+          },
+          {
+            id: 2,
+            action: "Add boiling water to sauce pan and drop in the noodles on low heat stir occationally. Place wok or frying pan on high heat, add a good swig of olive oil, place in the pork and all veg (not pak choi and garlic) and half the soy sauce",
+            duration: 5,
+            isComplete: false
+          },
+          {
+            id: 3,
+            action: "Keep stiring the contents of the pan, crush in the garlic and add pak choi",
+            duration: 5,
+            isComplete: false
+          },
+          {
+            id: 4,
+            action: "Drain noodles cut up with randomlly scissors, add to the wok with the rest of the soy sauce, mix everything together and ensure is cooked through",
+            duration: 3,
+            isComplete: false
+          },
+          {
+            id: 5,
+            action: "Bowl up, add more soy sauce to taste and enjoy.",
+            duration: null,
+            isComplete: false
+          },
+         
+        ],
+        tags:[
+          'pork', 'noodles', 'asian', 'healthy', 'vegetables', 'feel good', 'comfort food', 'weekend', 'saturday'
+        ],
+        isPublished: true,
+      },
+
     ],
   },
   getters:{
