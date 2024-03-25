641bd6eefc957bf9666d1b9e23cddc788ac6495a
Kerrial Newham <41723430+Kerrialn@users.noreply.github.com>
2024-03-25 22:46:50 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,28 +3,25 @@
     <div class="row justify-content-center mt-5">
       <div class="col-md-6 col-12">
         <div class="h4">What's my beef?</div>
-        <p>
-          I'm so tired of the endless tutorials where professional chef's make
-          recipes requiring expert knowledge and equipment all look so easy.
-        </p>
-        <p>
-          I'm also tired of making aforementioned recipes and wasting a bunch of
-          ingredients because it rarely works out. So fuck off with your 15
-          minute meals in a massive, fully equiped kitchen, with a professional
-          skill set.
-        </p>
+<p>
+  I'm so tired of the endless tutorials where professional chefs make recipes
+  requiring expert knowledge and equipment all look so easy.
+</p>
+<p>
+  I'm also tired of making aforementioned recipes and wasting a bunch of
+  ingredients because it rarely works out. So f&#k off with your 15-minute
+  meals in a massive, fully equipped kitchen, with a professional skill set.
+</p>
+<p>
+  Ultimately, I'm tired of the pretense. Home cooking isn't easy; it's a skill
+  that takes years to learn. You may have gathered, I'm no professional chef,
+  and nor do I have a large kitchen, nor is it fully equipped.
+</p>
+<p>
+  Alas, I also want to eat home-cooked, healthy meals, without breaking the
+  bank and having to go to culinary school to do so.
+</p>
 
-        <p>
-          Ultimately I'm tired of the pretense. Home cooking isn't easy, it's a
-          skill that takes years to learn. You may have gathered, I'm no
-          professional chef and nor do I have a large kitchen and nor is it
-          fully equipped.
-        </p>
-
-        <p>
-          Alas, I also want to eat home cooked, health meals, without breaking
-          the bank and having to go to culinary school to do so.
-        </p>
 
         <div class="h4">Mission</div>
         <p>
